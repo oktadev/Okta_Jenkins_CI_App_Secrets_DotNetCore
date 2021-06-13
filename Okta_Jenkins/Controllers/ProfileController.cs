6f2ed8f11d5d91189b25148939cc0f99c766e11a
nik <nik@fishbowlllc.com>
2021-06-13 12:34:54 -0400

    initail commit

---
+++ Okta_Jenkins/Controllers/ProfileController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Microsoft.AspNetCore.Authorization;
+
+namespace Okta_Jenkins.Controllers
+{
+    public class ProfileController : Controller
+    {
+        private readonly ILogger<ProfileController> _logger;
+
+        public ProfileController(ILogger<ProfileController> logger)
+        {
+            _logger = logger;
+        }
+
+        [Authorize]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
